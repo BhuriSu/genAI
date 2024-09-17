7035456268f18a61d0c0dd0d484aa61d42e863d9
BhuriSu <spryanun@gmail.com>
2024-09-17 23:34:38 +0700

    fix bug

--- bash_scripts/data_generator.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-source .venv/bin/activate  # Updated to use the virtual environment in the project root
-python bash_script/dataframe_to_kafka.py -i datasets/thailand_tourist.csv -t office_input -rst 2
